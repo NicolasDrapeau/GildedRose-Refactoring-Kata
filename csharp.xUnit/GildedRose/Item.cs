7315729cd1c5c37247ccd91a345089c12daa269b
Nicolas Drapeau <ndrapeau@luccasoftware.com>
2024-06-22 23:47:17 +0200

    first refacto

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -6,3 +6,16 @@ public class Item
     public int SellIn { get; set; }
     public int Quality { get; set; }
 }
+
+
+
+public static class Name
+{
+    public static readonly string AgedBrie = "Aged Brie";
+    public static readonly string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+    public static readonly string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    public static readonly string Dexterity = "+5 Dexterity Vest";
+    public static readonly string Elixir = "Elixir of the Mongoose";
+    public static readonly string Conjured = "Conjured Mana Cake";
+
+}
--- csharp.xUnit/GildedRose/Program.cs
+++ csharp.xUnit/GildedRose/Program.cs
@@ -11,36 +11,21 @@ public class Program
 
         IList<Item> items = new List<Item>
         {
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 15,
-                Quality = 20
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 10,
-                Quality = 49
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 5,
-                Quality = 49
-            },
+            new Item { Name = Name.Dexterity, SellIn = 10, Quality = 20 },
+            new Item { Name = Name.AgedBrie, SellIn = 2, Quality = 0 },
+            new Item { Name = Name.Elixir, SellIn = 5, Quality = 7 },
+            new Item { Name = Name.Sulfuras, SellIn = 0, Quality = 80 },
+            new Item { Name = Name.Sulfuras, SellIn = -1, Quality = 80 },
+            new Item { Name = Name.Backstage, SellIn = 15, Quality = 20 },
+            new Item { Name = Name.Backstage, SellIn = 10, Quality = 49 },
+            new Item { Name = Name.Backstage, SellIn = 5, Quality = 49 },
             // this conjured item does not work properly yet
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            new Item { Name = Name.Conjured, SellIn = 3, Quality = 6 }
         };
 
         var app = new GildedRose(items);
 
-        int days = 2;
+        var days = 2;
         if (args.Length > 0)
         {
             days = int.Parse(args[0]) + 1;
@@ -50,10 +35,11 @@ public class Program
         {
             Console.WriteLine("-------- day " + i + " --------");
             Console.WriteLine("name, sellIn, quality");
-            for (var j = 0; j < items.Count; j++)
+            foreach (var t in items)
             {
-                Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
+                Console.WriteLine(t.Name + ", " + t.SellIn + ", " + t.Quality);
             }
+
             Console.WriteLine("");
             app.UpdateQuality();
         }
